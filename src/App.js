4be10bfe7b0b4768ed7c4dc8bba4f968d6e34e7d
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2024-03-02 16:26:55 -0800

    typography setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div>Hello</div>;
-}
+import "./App.css";
 
+function App() {
+  return (
+    <div>
+      <h1>Urna Molestie At Elementum Eu Facilisis Sed Odio</h1>
+      <h2>Urna Molestie At Elementum Eu Facilisis Sed Odio</h2>
+      <h3>Urna Molestie At Elementum Eu Facilisis Sed Odio</h3>
+      <h4>Urna Molestie At Elementum Eu Facilisis Sed Odio</h4>
+      <h5>Urna Molestie At Elementum Eu Facilisis Sed Odio</h5>
+      <h6>Urna Molestie At Elementum Eu Facilisis Sed Odio</h6>
+      <p>Urna Molestie At Elementum Eu Facilisis Sed Odio</p>
+      <small>Urna Molestie At Elementum Eu Facilisis Sed Odio</small>
+      <p className="caption">
+        Urna Molestie At Elementum Eu Facilisis Sed Odio
+      </p>
+    </div>
+  );
+}
 export default App;
