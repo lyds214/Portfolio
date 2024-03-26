69a663f4a4c077dd44751300d8b82f3e9df63706
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2024-03-25 20:47:44 -0700

    added hover and pressed button states

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import BeachHacks from "./pages/projects/BeacHacks";
-import Hermes from "./pages/projects/Hermes";
-import Quickify from "./pages/projects/Quickify";
+// import Contact from "./pages/Contact";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import BeachHacks from "./pages/projects/BeacHacks";
+// import Hermes from "./pages/projects/Hermes";
+// import Quickify from "./pages/projects/Quickify";
 import Header from "./pages/components/Header";
+import SideBar from "./pages/components/SideBar";
 
 function App() {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
+      <SideBar />
     </div>
   );
 }
--- src/pages/components/SideBar.js
+++ src/pages/components/SideBar.js
@@ -1,7 +1,65 @@
 import React from "react";
+import { useState } from "react";
+
+export const navData = [
+  { id: 0, text: "main", link: "#" },
+  { id: 1, text: "about", link: "#about" },
+  { id: 2, text: "projects", link: "#projects" },
+  { id: 3, text: "contact", link: "#contact" },
+];
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  const [isPressed, setIsPressed] = useState(false);
+  const [isHover, setHover] = useState(false);
+
+  // change to pressed button style if it's in default button style
+  // when clicked again in pressed button style, it'll stays in pressed button style
+  const handleClick = () => {
+    if (!isPressed) {
+      setIsPressed(!isPressed);
+    }
+  };
+
+  const handleHover = () => {
+    setHover(true);
+  };
+
+  const handleLeave = () => {
+    setHover(false);
+  };
+
+  // default button style
+  const defaultBtnStyle = {
+    height: isHover ? "14px" : "8px",
+    width: isHover ? "14px" : "8px",
+    borderWidth: "1px",
+    border: "solid",
+    borderColor: "var(--primary-color)",
+    backgroundColor: "var(--secondary-color)",
+    borderRadius: "50%",
+    transition: "width 0.3s, height 0.3s",
+  };
+
+  // pressed button style
+  const pressedBtnStyle = {
+    height: isHover ? "20px" : "14px",
+    width: isHover ? "20px" : "14px",
+    border: "solid",
+    borderWidth: "1px",
+    backgroundColor: "var(--primary-color)",
+    borderRadius: "50%",
+    borderColor: "var(--secondary-color)",
+    transition: "width 0.3s, height 0.3s",
+  };
+
+  return (
+    <div
+      style={isPressed ? pressedBtnStyle : defaultBtnStyle}
+      onClick={handleClick}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleLeave}
+    ></div>
+  );
 };
 
 export default SideBar;
