2279265b3c41c531b0d33aed31b2d215551e7804
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2024-03-26 23:32:16 -0700

    starting home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
-import Header from "./pages/components/Header";
-import SideBar from "./pages/components/SideBar/SideBar";
+
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div>
       {/* <Header /> */}
-      <SideBar />
+      {/* <SideBar /> */}
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>LYDIA YANG</h1>
+      <button>CONTINUE</button>
+    </div>
+  );
+};
+
+export default Home;
