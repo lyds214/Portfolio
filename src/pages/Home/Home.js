10047c17b703bd1566fdd7b4139e87319cebebf6
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2024-03-27 22:13:47 -0700

    finished aligning the name, continue button, and the underline

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,34 @@
 import React from "react";
 import styles from "./Home.module.css";
+import ellipse from "../../assets/ellipse.svg";
 
 const Home = () => {
   return (
-    <div>
-      <h1>LYDIA YANG</h1>
-      <button>CONTINUE</button>
-    </div>
+    <>
+      <div className={styles.container}>
+        <h1>LYDIA YANG</h1>
+        <div className={styles.underline}>
+          <div className={styles.diamond} />
+          <div className={styles.line} />
+          <div className={styles.diamond} />
+        </div>
+        <button>CONTINUE</button>
+      </div>
+    </>
   );
 };
 
 export default Home;
+{
+  /* <>
+<img src={ellipse} />
+<div className={styles.outerCircle}>
+  <div className={styles.circle2}>
+    <div className={styles.circle1}>
+      <h1>LYDIA YANG</h1>
+      <button>CONTINUE</button>
+    </div>
+  </div>
+</div>
+</> */
+}
