31ffd80f31915bf38f706d7fb59b46a779a7cc49
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2024-03-29 22:51:32 -0700

    finished circle name

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,31 +4,25 @@ import ellipse from "../../assets/ellipse.svg";
 
 const Home = () => {
   return (
-    <>
-      <div className={styles.container}>
-        <h1>LYDIA YANG</h1>
-        <div className={styles.underline}>
-          <div className={styles.diamond} />
-          <div className={styles.line} />
-          <div className={styles.diamond} />
+    <div className={styles.container}>
+      <img src={ellipse} />
+      <div>
+        <div className={styles.circle2}>
+          <div className={styles.circle1}>
+            <div className={styles.nameContainer}>
+              <h1>LYDIA YANG</h1>
+              <div className={styles.underline}>
+                <div className={styles.diamond} />
+                <div className={styles.line} />
+                <div className={styles.diamond} />
+              </div>
+              <button>CONTINUE</button>
+            </div>
+          </div>
         </div>
-        <button>CONTINUE</button>
       </div>
-    </>
+    </div>
   );
 };
 
 export default Home;
-{
-  /* <>
-<img src={ellipse} />
-<div className={styles.outerCircle}>
-  <div className={styles.circle2}>
-    <div className={styles.circle1}>
-      <h1>LYDIA YANG</h1>
-      <button>CONTINUE</button>
-    </div>
-  </div>
-</div>
-</> */
-}
