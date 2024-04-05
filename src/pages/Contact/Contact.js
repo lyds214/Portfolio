d0ae2f0a3195490cd40c66a06ef1d843cd3210f0
Lydia Yang <lydiayang92@gmail.com>
2024-04-04 17:43:13 -0700

    trying to put image inside the content container

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./Contact.module.css";
 import "../../App.css";
 import resume from "../../assets/icons/resume/Resume36.svg";
-import email from "../../assets/icons/email/Email36.svg";
-import linkedin from "../../assets/icons/linkedin/LinkedIn36.svg";
-import github from "../../assets/icons/github/GitHub36.svg";
+// import email from "../../assets/icons/email/Email36.svg";
+// import linkedin from "../../assets/icons/linkedin/LinkedIn36.svg";
+// import github from "../../assets/icons/github/GitHub36.svg";
 
 const Contact = () => {
+  // const images = [resume, email, linkedin, github];
+
   return (
     <>
       <div className={styles.header}>
@@ -20,14 +22,10 @@ const Contact = () => {
       </div>
 
       <div className={styles.container}>
-        <div className={styles.content}>
-          <h1>Keep In Touch</h1>
-          <h4>I'm open! Please reach out if you're interested!</h4>
-        </div>
-        {/* <img src={resume} alt="resume" />
-        <img src={email} alt="email" />
-        <img src={linkedin} alt="linkedin" />
-        <img src={github} alt="github" /> */}
+        <h1 style={{ margin: "0px" }}>Keep In Touch</h1>
+        <h4 style={{ marginTop: "18px", marginBottom: "0px" }}>
+          I'm open! Please reach out if you're interested!
+        </h4>
       </div>
     </>
   );
